28f32613aabfd22c0452bbc04657eb3ddf87e579
JarvisLi <jarvisli@huaxing.com>
2018-04-23 02:43:05 +0800

    a

--- lib/audited/audit.rb
+++ lib/audited/audit.rb
@@ -39,8 +39,8 @@ module Audited
     belongs_to :associated, polymorphic: true
 
     def auditable_type=(sType)
-      binding.pry
       super(sType)
+      super(auditable.class.name)
     end
 
     before_create :set_version_number, :set_audit_user, :set_request_uuid, :set_remote_address
